a4486a092a628472a321148fba5f346faa3a557d
Alex Magana <cheekywithcheeky@gmail.com>
2024-03-08 01:46:03 +0000

    new

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+//import { Routes ,Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+        <Route path='/' exact element={ <Home />}></Route>
+          <Route path='/services' element={Services} />
+          <Route path='/products' element={Products} />
+          <Route path='/sign-up' element={SignUp} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
